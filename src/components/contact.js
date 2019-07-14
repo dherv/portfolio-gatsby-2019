b83c8e82d3c02c4fbd5dc4f36cdbb2ca56a44715
dherv <damien.herv@gmail.com>
2019-07-15 06:52:17 +0900

    feature(copy_clipboard) - Add copy email to clipboard

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,17 +4,30 @@ import styles from "./contact.module.css"
 import Submit from "../components/submit"
 
 const Contact = () => {
+  const handlCopyClipboard = () => {
+    navigator.clipboard.writeText("damien.herv@gmail.com")
+  }
   return (
     <section id="contact" className={styles.container}>
-      <div className={styles.wrapper}>
+      <form
+        action="https://formspree.io/damien.herv@gmail.com"
+        method="POST"
+        className={styles.wrapper}
+      >
         <h2 className="global_section_title">Contact</h2>
         <p className={styles.description}>
-          You can drop a line or copy my email address here. They will both
-          arrive a the same place.
+          You can drop a line or{" "}
+          <span
+            className={styles.copy_to_clipboard}
+            onClick={() => handlCopyClipboard()}
+          >
+            copy
+          </span>{" "}
+          my email address.
         </p>
-        <textarea rows={8} className={styles.textbox} />
-        <button className={styles.btn_submit}>submit</button>
-      </div>
+        <textarea rows={8} className={styles.textbox} name="content" />
+        <input className={styles.btn_submit} type="submit" value="Send" />
+      </form>
     </section>
   )
 }
